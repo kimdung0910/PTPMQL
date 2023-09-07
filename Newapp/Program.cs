e2691ae69dd49a8a2cb74de6a009eb52bf796607
kimdung0910 <kimdung09102002@gmail.com>
2023-09-07 17:43:26 +0700

    commit note

---
+++ Newapp/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhap vao du lieu:");
+        int a = Console.Read();
+        Console.WriteLine("Du lieu vua nhap vao la: " + a);
+    }
+}
