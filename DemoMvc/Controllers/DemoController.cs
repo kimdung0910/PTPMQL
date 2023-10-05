d7ad8387ef3a4166ca72a4dd8c3f8278df0f8f76
kimdung0910 <kimdung09102002@gmail.com>
2023-10-05 19:15:48 +0700

    commit note

---
+++ DemoMvc/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+
+namespace DemoMvc.Controllers
+{
+    public class DemoController : Controller
+    {
+     //Khai bao cac phuong thuc
+     //Tran Thi Kim Dung - 2021050135
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+         public IActionResult Index(string fName, string mEmail)
+        {
+            string strResult = "Hello " + fName + "-" + mEmail; // Sửa lỗi ở đây, đúng cách khai báo biến
+            ViewBag.thongBao = strResult; // Sử dụng ViewBag để truyền dữ liệu đến View
+            return View(); // Tạo một cái view tương ứng với action
+        }
+    }
+}
